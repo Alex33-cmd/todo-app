2b889905d674ac50e300ba8680b6badfcecb149d
Alex33-cmd <62179831+Alex33-cmd@users.noreply.github.com>
2022-04-18 17:19:38 +0300

    loading logic and animation added

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -7,23 +7,28 @@
         @add-todo="addTodo"
     />
     <hr>
+    <Loader v-if="loading" />
     <TodoList
+        v-else-if="todos.length"
         v-bind:todosData="todos"
         @remove-todo="removeTodo"
     />
+    <p v-else>No to-dos!</p>
     </div>
 </template>
 
 <script>
 import TodoList from '../components/TodoList.vue'
 import AddTodo from '../components/AddTodo.vue'
+import Loader from '../components/Loader.vue'
 
 export default {
   name: 'app',
   // application data storage
   data() {
     return {
-      todos: []
+      todos: [],
+      loading: true
     }
   },
 
@@ -32,10 +37,16 @@ export default {
     // fetch data and fill todo array with it
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
       .then(response => response.json())
-      .then(json => {this.todos = json})
+      .then(json => {
+          setTimeout(()=> {
+            this.todos = json
+            this.loading = false
+          }, 2000)
+          
+          })
   },
 
-  components: {TodoList, AddTodo},
+  components: {TodoList, AddTodo, Loader},
 
   methods: {
     removeTodo(id){
