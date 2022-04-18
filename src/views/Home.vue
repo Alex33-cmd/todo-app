91eeb3899c30d88d3e7e9dcf9f36b3edbcf77d96
Alex33-cmd <62179831+Alex33-cmd@users.noreply.github.com>
2022-04-18 16:39:54 +0300

    router-links added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,6 @@
     <div>
         <h2>Home page</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ut ea, necessitatibus sit magnam ducimus harum. Aut ratione labore id! Omnis consequatur vel quibusdam? Facere, voluptates! Autem, minima. Autem, iste?</p>
-        <a href="/todos">Todos</a>
+        <router-link to="/todos">Todos</router-link>
     </div>
 </template>
